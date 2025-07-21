7c2ed6e6b0f9c0f77bf149cf2521ac52f266d564
Kieran Powell <proton5@live.com>
2025-07-21 17:17:31 +0100

    Add Celtic Scroll

---
+++ scripts/celticBorderScroll.js
@@ -0,0 +1,30 @@
+const CelticTileLocation = "media/celtic border.svg";
+
+function CelticBorderScroll() {
+  if (window.innerWidth < 622) {
+    return;
+  }
+
+  let borders = [...document.getElementsByClassName("celtic-border")];
+  let borderContainer = [
+    ...document.getElementsByClassName("border-container"),
+  ];
+  const footer = document.getElementById("footer");
+  const footerY = footer.getBoundingClientRect().bottom + window.scrollY;
+
+  borderContainer.forEach((bc) => {
+    bc.style.height = footerY;
+  });
+
+  borders.forEach((border) => {
+    for (let i = 1; i <= footerY / 50; i++) {
+      let celticTile = document.createElement("img");
+      celticTile.src = CelticTileLocation;
+      celticTile.className = "celtic-tile";
+      celticTile.style.top = i * 267;
+      border.appendChild(celticTile);
+    }
+  });
+}
+
+window.onload = CelticBorderScroll;
